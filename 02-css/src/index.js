4f4193d3f187f93427e082dc9688fb896d41aedb
wangding <408542507@qq.com>
2022-01-02 14:23:26 +0800

    02-css

---
+++ 02-css/src/index.js
@@ -0,0 +1,5 @@
+import sayHello from './lib.js';
+import './layout.css';
+
+document.body.innerHTML = '<h1>Hello webpack!</h1>';
+sayHello();
---
+++ 02-css/webpack.config.js
@@ -0,0 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    clean: true
+  },
+  mode: 'none',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+      //use: [MiniCssExtractPlugin.loader, 'css-loader']
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'hello webpack',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin()]
+};
